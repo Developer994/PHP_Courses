e74c2d6c41884779b63965354e755347b63126d0
Silent Dev <rez_a@outlook.com>
2020-02-12 18:55:45 -0500

    Finished the conditional statements lesson.

--- Net_Ninja/tutorial.php
+++ Net_Ninja/tutorial.php
@@ -156,9 +156,42 @@
     // echo 5 == '5'; // Two equal signs would be loose equal, which means 5 and '5' are loosely equal
     //                // but not strictly equal.
 
-    echo 5 === '5'; // This will check if both the digits are ints or not, as it wil check whether or not
-                    // they're strictly equal.
+    // echo 5 === '5'; // This will check if both the digits are ints or not, as it wil check whether or not
+    //                 // they're strictly equal.
     
+    // Conditional Statements
+
+    // $price = 60;
+
+    // if ($price < 10){
+    //     echo 'the condition is met';
+    // } elseif ($price < 20){
+    //     echo 'elseif condition met';
+    // }
+    //  else {
+    //     echo 'the condition is not met';
+    // }
+
+// // Or (||) and And (&&) examples with foreach loops and if statements
+// $products = [
+//         ['name' => 'shiny star', 'price' => 20],
+//         ['name' => 'green shell', 'price' => 10],
+//         ['name' => 'red shell', 'price' => 15],
+//         ['name' => 'gold coin', 'price' => 5],
+//         ['name' => 'lightning bolt', 'price' => 40],
+//         ['name' => 'banana skin', 'price' => 2]
+//     ];
+
+//     foreach($products as $product){
+//             // And percent (&&)
+//             // if($product['price'] < 15 && $product['price'] > 2){
+//             //     echo $product['name'] . '<br/>';
+//             // }
+//             // Or (||)
+//             if($product['price'] > 20 || $product['price'] < 10){
+//                 echo $product['name'] . '<br/>';
+//             }
+//     }
 
 ?>
 
@@ -173,6 +206,22 @@
     <title>PHP Tutorials</title>
 </head>
 <body>
-
+    <div><ul>
+    <?php 
+    $products = [
+        ['name' => 'shiny star', 'price' => 20],
+        ['name' => 'green shell', 'price' => 10],
+        ['name' => 'red shell', 'price' => 15],
+        ['name' => 'gold coin', 'price' => 5],
+        ['name' => 'lightning bolt', 'price' => 40],
+        ['name' => 'banana skin', 'price' => 2]
+    ];
+    ?>
+    <?php foreach($products as $product){ ?>
+    <?php if($product['price'] > 15) {?>
+        <li><?php echo $product['name'];?></li>
+    <?php }?>
+    <?php } ?>
+    </ul></div>
 </body>
 </html>
