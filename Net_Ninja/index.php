b5dca6333b35a998f74d8923bc48fd1f4b61ce56
Silent Dev <rez_a@outlook.com>
2020-02-19 12:17:10 -0500

    Added the explode function to turn the ingredients into an array of strings.

--- Net_Ninja/index.php
+++ Net_Ninja/index.php
@@ -1,5 +1,12 @@
 <?php
 
+    // We've created a database using the workbench that you can use by clicking on
+    // admin when you're on the xampp control panel and then you can create some data,
+    // which is already created and is used by this index.php file.
+    // We can use the index.php file to connect to the database and do all sorts of things
+    // such as fetching the data and outputting the data in a listed list through the HTML
+    // portion.
+
     // connect to database
     $conn = mysqli_connect('localhost', 'lolter', 'Test1234', 'ninja_pizza');
 
@@ -25,6 +32,10 @@
 
     // print_r($pizzas);
 
+    // The explode function takes in two arguments (The first one would be some character to look at and seperate it,
+    // and the second one would look at the string that needs to be targeted.)
+    print_r(explode(',', $pizzas[0]['ingredients']));
+
 ?>
 
 <!DOCTYPE html>
@@ -36,13 +47,17 @@
     <div class="container">
         <div class="row">
         
-        <?php foreach($pizzas as $pizza){ ?>
+        <?php foreach($pizzas as $pizza): ?>
 
             <div class="col s6 md3">
                 <div class="card z-depth-0">
                     <div class="card-content center">
                         <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-                        <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                        <ul>
+                        <?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
+                            <li><?php echo htmlspecialchars($ing) ?></li>
+                        <?php endforeach ?>
+                        </ul>
                     </div>
                     <div class="card-action right-align">
                         <a href="#" class="brand-text">more info</a>
@@ -50,8 +65,15 @@
                 </div>
             </div>
 
-        <?php } ?>
+        <?php endforeach; ?>
         
+        <?php if(count($pizzas)>=3): ?>
+            <p> There are 3 or more pizzas</p>
+        <?php else: ?>
+            <p>There are less than 3 pizzas</p>
+        <?php endif; ?>
+
+
         </div>
     </div>
 
