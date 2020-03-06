d8af381db39a662c7ae2a306c27340e84f1efef0
Silent Dev <rez_a@outlook.com>
2020-03-06 12:22:00 -0500

    Changed name & email to private and added name verification so that the user can't add an invalid name such as a number.

--- Net_Ninja/tutorial.php
+++ Net_Ninja/tutorial.php
@@ -375,6 +375,47 @@
     
     // // Deleting a file
     // unlink($file);
+
+    // Classes 
+
+    class User {
+
+        private $email;
+        private $name;
+
+        public function __construct($name, $email){
+            // $this->email = 'mario@thenetninja.co.uk';
+            // $this->name = 'mario';
+            $this->email =  $email;
+            $this->name = $name;
+        }
+
+        public function login(){
+            // echo 'the user logged in. ';
+            echo $this->name . ' logged in';
+        }
+
+        public function getName(){
+            return $this->name;
+        }
+
+        public function setName($name){
+            if(is_string($name) && strlen($name) > 1){
+                $this->name = $name;
+                return "Name has been updated to $name";
+            } else {
+                return 'Not a valid name';
+            }
+        }
+
+    }
+    $userTwo = new User('Yoshi', 'yoshi@thenetninjas.co.uk');
+
+    // echo $userTwo->getName();
+    // echo $userTwo->setName('50');
+    echo $userTwo->setName('Shaun');
+    echo $userTwo->getName();
+
 ?>
 
 
